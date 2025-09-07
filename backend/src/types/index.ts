de1d9fe8ef1609452cf8270498c75bc208731d1a
swastikCommits <textswastik.alt@gmail.com>
2025-09-07 19:49:23 +0530

    updated login schema

--- backend/src/types/index.ts
+++ backend/src/types/index.ts
@@ -5,7 +5,7 @@ import { WebSocket } from "ws";
 
 export const loginSchema = z.object({
     email: z.string().email("Invalid email address"),
-    password: z.string().min(1, "Password is required") 
+    password: z.string().min(6, "Password is required") 
 });
 
 export const signupSchema = z.object({
